6da24e1451537c33e51ca23adf5d04f101dba2d9
Felipe Pereira De Britto <162499079+Cxrzin08@users.noreply.github.com>
2024-06-05 20:49:48 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from conversor_medidas import ConversorMedidas
 
 print('''
     [0]. Sair
-    [1]. Temperatura - C para F
+    [1]. Temperatura - Celsius para Fahrenheit
+    [2]. Temperatura - Fahrenheit para Celsius
       ''')
 selecao = input("Digite uma opção: ")
 
@@ -14,4 +15,8 @@ if selecao == "0":
 if selecao == "1":
     valor_c = float(input("Qual a temperatura em celcius que deseja converter?"))
     resultado = Temperatura.c_para_f(valor_c)
+    print(f"A temperatura convertida é : {resultado}")
+if selecao == "2"
+    valor_f = float(input("Qual a temperatura em fahrenheit que deseja converter?"))
+    resultado = Temperatura.f_para_c(valor_f)
     print(f"A temperatura convertida é : {resultado}")
