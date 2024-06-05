c2d0f2b4f6ba320d80ecb377ab4b8e98ceb42d2e
Felipe Perteira De Britto <felipepereiradebritto08@gmail.com>
2024-06-05 19:35:36 -0300

    Rafatoração de código

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -1,39 +1,11 @@
 class Temperatura:
-    def _init_(self) -> None:
+    def _init_(self):
         pass
 
-while True:
-    converter = input("Qual opção você deseja?\n1. Converter de Fahrenheit para Celsius.\n2. Converter de Celsius para Fahrenheit.\n")
-    if converter in ["1", "2"]:
-        break
-    else:
-        print("Opção inválida. Por favor, escolha 1 ou 2.")
-    
-while True:
-    oi2 = input("Pressione Enter para continuar...")
-    if oi2 == "":
-        break
-    else:
-        print("Por favor, pressione apenas 'Enter'.")
+    def c_para_f():
+        pass
 
-if converter == "1":
-    fahrenheit = float(input("Digite a temperatura em Fahrenheit:\n"))
-    celsius = (fahrenheit - 32) * 5/9
-    print(f"A temperatura convertida é {celsius:.2f} graus Celsius.")
-elif converter == "2":
-    celsius = float(input("Digite a temperatura em Celsius:\n"))
-    fahrenheit = (celsius * 9/5) + 32
-    print(f"A temperatura convertida é {fahrenheit:.2f} graus Fahrenheit.")
+    def f_para_c():
+        pass
 
-while True:
-    teste2 = input("Deseja voltar para a tela inicial?\n1. Voltar para a tela inicial\n2. Encerrar o programa\n")
-    if teste2 in ["1", "2"]:
-        break
-    else:
-        print("Selecione uma opção válida")
-if teste2 == "2":
-    print("Programa encerrado.")
-    exit()
-elif teste2 == "1":
-    import limpartexto
-    import main
+
