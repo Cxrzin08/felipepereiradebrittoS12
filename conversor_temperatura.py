a93ddc16f12da0384a87cb035860bb8e7e707997
Felipe Perteira De Britto <felipepereiradebritto08@gmail.com>
2024-06-05 19:47:13 -0300

    Implement c for f

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -2,10 +2,16 @@ class Temperatura:
     def _init_(self):
         pass
 
-    def c_para_f():
-        pass
+    def c_para_f(valor_c):
+        resultado = valor_c * 1.8 + 32
+        return resultado
 
     def f_para_c():
         pass
+
+
+
+teste = Temperatura.c_para_f(0)
+print(f"{teste}")
 
 
