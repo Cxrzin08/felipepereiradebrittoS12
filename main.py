53bf87e423ce5ef897726fba4612eb75fffff57d
Felipe Perteira De Britto <felipepereiradebritto08@gmail.com>
2024-06-05 20:10:55 -0300

    Saida arrumada

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ print('''
 selecao = input("Digite uma opção: ")
 
 
+if selecao == "0":
+    print("Programa encerrado")
+    exit()
 if selecao == "1":
     valor_c = float(input("Qual a temperatura em celcius que deseja converter?"))
     resultado = Temperatura.c_para_f(valor_c)
-    print(f"A temperatura convertida é :{resultado}")
+    print(f"A temperatura convertida é : {resultado}")
