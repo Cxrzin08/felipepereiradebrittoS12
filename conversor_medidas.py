746b9b512f2a566d077a5a4e20a1b4ebfe84d78f
Felipe Pereira De Britto <162499079+Cxrzin08@users.noreply.github.com>
2024-06-05 20:55:57 -0300

    conversor pronto

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -2,3 +2,13 @@ class ConversorMedidas:
     def __init__(self) -> None:
         pass
 
+    def cm_para_m(valor_cm):
+        resultado = valor_cm / 100
+        return resultado
+
+    def m_para_cm(valor_m):
+        resultado = valor_m * 100
+        return resultado
+
+
+
