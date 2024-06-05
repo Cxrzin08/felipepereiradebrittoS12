58ff16258d0c1f575d02e46e9bca7a4050113408
Felipe Pereira De Britto <162499079+Cxrzin08@users.noreply.github.com>
2024-06-05 20:59:01 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ selecao = input("Digite uma opção: ")
 if selecao == "0":
     print("Programa encerrado")
     exit()
-if selecao == "1":
+elif selecao == "1":
     valor_c = float(input("Qual a temperatura em celcius que deseja converter?"))
     resultado = Temperatura.c_para_f(valor_c)
     print(f"A temperatura convertida é : {resultado}")
-if selecao == "2"
+elif selecao == "2"
     valor_f = float(input("Qual a temperatura em fahrenheit que deseja converter?"))
     resultado = Temperatura.f_para_c(valor_f)
     print(f"A temperatura convertida é : {resultado}")
