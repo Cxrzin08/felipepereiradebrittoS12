0c022bfa8c103dbac9a752245ec8325a5443a68c
Felipe Pereira De Britto <162499079+Cxrzin08@users.noreply.github.com>
2024-06-05 22:38:56 -0300

    Fiz bug

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -3,9 +3,9 @@ class ConversorMedidas:
         pass
 
     def cm_para_m(valor_cm):
-        resultado = valor_cm / 100
+        resultado = valor_cm * 100
         return resultado
 
     def m_para_cm(valor_m):
-        resultado = valor_m * 100
+        resultado = valor_m / 100
         return resultado
