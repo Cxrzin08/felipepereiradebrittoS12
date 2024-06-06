f4e5f2bdcd093f6370e693ccacd75f66278f8607
Felipe Pereira De Britto <162499079+Cxrzin08@users.noreply.github.com>
2024-06-05 22:06:20 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ print('''
     [0]. Sair
     [1]. Temperatura - Celsius para Fahrenheit
     [2]. Temperatura - Fahrenheit para Celsius
+    [3]. Converter medidas - Centímetros para Metros
+    [4]. Converter medidas - Metros para Centímetros
       ''')
 selecao = input("Digite uma opção: ")
 
@@ -20,3 +22,9 @@ elif selecao == "2"
     valor_f = float(input("Qual a temperatura em fahrenheit que deseja converter?"))
     resultado = Temperatura.f_para_c(valor_f)
     print(f"A temperatura convertida é : {resultado}")
+elif selecao == "3":
+    valor_cm = float(input("Digite os metros que  deseja converter":))
+resultado =      ConversorMedidas.cm_para_m(valor_cm)
+
+elif selecao == "4":
+    valor_m = float(input("Digite os centímetros que deseja converter:"))
