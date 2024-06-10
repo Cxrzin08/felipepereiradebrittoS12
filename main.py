40fc0719d3ecb97c16cb65f22ca5468d5f461ee8
Felipe Perteira De Britto <felipepereiradebritto08@gmail.com>
2024-06-10 15:35:59 -0300

    fix bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from conversor_temperatura import Temperatura
 from conversor_medidas import ConversorMedidas
 
 def reiniciar():
-    print("Olá sou o programa do Felipe, digite qual opção você deseja")
+    print("Olá, sou o programa do Felipe. Digite qual opção você deseja:")
     print('''
     [0]. Sair
     [1]. Temperatura - Celsius para Fahrenheit
@@ -21,61 +21,60 @@ while True:
     reiniciar()
     selecao = input("Digite uma opção: ")
     if selecao in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]:
-        break
-    else:
-        print("Opção inválida, por favor digite uma opção válida")
+        if selecao == "0":
+            print("Programa encerrado.")
+            break
 
-match selecao:
-    case "0":
-        print("Programa encerrado.")
-        exit()
-    case "1":
-        valor = float(input("Digite o valor da temperatura: "))
-        resultado = Temperatura.c_para_f(valor)
-        print(f"A temperatura convertida é: {resultado} Fahrenheit")
-    case "2":
-        valor = float(input("Digite o valor da temperatura: "))
-        resultado = Temperatura.f_para_c(valor)
-        print(f"A temperatura convertida é: {resultado} Celsius")
-    case "3":
-        valor = float(input("Digite o valor da temperatura: "))
-        resultado = Temperatura.k_para_c(valor)
-        print(f"A temperatura convertida é: {resultado} Celsius")
-    case "4":
-        valor = float(input("Digite o valor da temperatura: "))
-        resultado = Temperatura.c_para_k(valor)
-        print(f"A temperatura convertida é: {resultado} Kelvin")
-    case "5":
-        valor = float(input("Digite o valor da temperatura: "))
-        resultado = Temperatura.k_para_f(valor)
-        print(f"A temperatura convertida é: {resultado} Fahrenheit")
-    case "6":
-        valor = float(input("Digite o valor da temperatura: "))
-        resultado = Temperatura.f_para_k(valor)
-        print(f"A temperatura convertida é: {resultado} Kelvin")
-    case "7":
-        valor = float(input("Digite o valor para a conversão: "))
-        resultado = ConversorMedidas.cm_para_m(valor)
-        print(f"Os centímetros convertidos para metros é: {resultado} Metros")
-    case "8":
-        valor = float(input("Digite o valor para a conversão: "))
-        resultado = ConversorMedidas.m_para_cm(valor)
-        print(f"Os metros convertidos para centímetros é: {resultado} Centímetros")
-    case "9":
-        valor = float(input("Digite o valor para a conversão: "))
-        resultado = ConversorMedidas.m_para_km(valor)
-        print(f"Os metros convertidos para kilômetros é: {resultado} Kilômetros")
-    case "10":
-        valor = float(input("Digite o valor para a conversão: "))
-        resultado = ConversorMedidas.km_para_m(valor)
-        print(f"Os kilômetros convertidos para metros é: {resultado} metros")
+        match selecao:
+            case "1":
+                valor = float(input("Digite o valor da temperatura: "))
+                resultado = Temperatura.c_para_f(valor)
+                print(f"A temperatura convertida é: {resultado} Fahrenheit")
+            case "2":
+                valor = float(input("Digite o valor da temperatura: "))
+                resultado = Temperatura.f_para_c(valor)
+                print(f"A temperatura convertida é: {resultado} Celsius")
+            case "3":
+                valor = float(input("Digite o valor da temperatura: "))
+                resultado = Temperatura.k_para_c(valor)
+                print(f"A temperatura convertida é: {resultado} Celsius")
+            case "4":
+                valor = float(input("Digite o valor da temperatura: "))
+                resultado = Temperatura.c_para_k(valor)
+                print(f"A temperatura convertida é: {resultado} Kelvin")
+            case "5":
+                valor = float(input("Digite o valor da temperatura: "))
+                resultado = Temperatura.k_para_f(valor)
+                print(f"A temperatura convertida é: {resultado} Fahrenheit")
+            case "6":
+                valor = float(input("Digite o valor da temperatura: "))
+                resultado = Temperatura.f_para_k(valor)
+                print(f"A temperatura convertida é: {resultado} Kelvin")
+            case "7":
+                valor = float(input("Digite o valor para a conversão: "))
+                resultado = ConversorMedidas.cm_para_m(valor)
+                print(f"Os centímetros convertidos para metros é: {resultado} Metros")
+            case "8":
+                valor = float(input("Digite o valor para a conversão: "))
+                resultado = ConversorMedidas.m_para_cm(valor)
+                print(f"Os metros convertidos para centímetros é: {resultado} Centímetros")
+            case "9":
+                valor = float(input("Digite o valor para a conversão: "))
+                resultado = ConversorMedidas.m_para_km(valor)
+                print(f"Os metros convertidos para kilômetros é: {resultado} Kilômetros")
+            case "10":
+                valor = float(input("Digite o valor para a conversão: "))
+                resultado = ConversorMedidas.km_para_m(valor)
+                print(f"Os kilômetros convertidos para metros é: {resultado} Metros")
 
-while True:
-    teste = input("\n\nDeseja voltar para a tela inicial? Se sim, digite (Sim) \nDeseja encerrar o programa? Se sim digite (Encerrar)\n").capitalize()
-    if teste == "Sim":
-        break
-    elif teste == "Encerrar":
-        print("Programa encerrado.")
-        exit()
+        while True:
+            teste = input("\n\nDeseja voltar para a tela inicial? Se sim, digite (Sim)\nDeseja encerrar o programa? Se sim, digite (Encerrar): ").capitalize()
+            if teste == "Sim":
+                break
+            elif teste == "Encerrar":
+                print("Programa encerrado.")
+                exit()
+            else:
+                print("Por favor, digite uma opção correspondente.")
     else:
-        print("Por favor digite uma opção correspondente.")
+        print("Opção inválida, por favor digite uma opção válida.")
